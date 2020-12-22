a067b19ec19e46a15270019de6a0a7005b9da668
lukagregorn <gregorn.luka@gmail.com>
2020-12-22 11:40:51 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,88 @@
+import Application from './src/Application.js';
+import * as WebGL from './src/WebGL.js';
+import GLTFLoader from './src/GLTFLoader.js';
+import Renderer from './src/Renderer.js';
+
+const mat4 = glMatrix.mat4;
+
+class App extends Application {
+
+    async start() {
+        this.loader = new GLTFLoader();
+        await this.loader.load('./assets/main_scene/main_scene.gltf');
+
+        this.scene = await this.loader.loadScene(this.loader.defaultScene);
+        this.camera = await this.loader.loadNode('Camera');
+        this.player = await this.loader.loadNode('Player');
+
+        if (!this.scene || !this.camera) {
+            throw new Error('Scene or Camera not present in glTF');
+        }
+
+        if (!this.camera.camera) {
+            throw new Error('Camera node does not contain a camera reference');
+        }
+
+        this.renderer = new Renderer(this.gl);
+        this.renderer.prepareScene(this.scene);
+        this.time = Date.now();
+        this.startTime = this.time;
+        this.aspect = 1;
+
+        this.pointerlockchangeHandler = this.pointerlockchangeHandler.bind(this);
+        document.addEventListener('pointerlockchange', this.pointerlockchangeHandler);
+
+        this.resize();
+    }
+
+    enableCamera() {
+        this.canvas.requestPointerLock();
+    }
+
+    pointerlockchangeHandler() {
+        if (!this.camera) {
+            return;
+        }
+
+        if (document.pointerLockElement === this.canvas) {
+            //this.camera.camera.enable();
+        } else {
+            //this.camera.camera.disable();
+        }
+    }
+
+    update() {
+        const t = this.time = Date.now();
+        const dt = (this.time - this.startTime) * 0.001;
+        this.startTime = this.time;
+
+        if (this.camera) {
+            // this.camera.camera.update(dt);
+        }
+    }
+
+    render() {
+        if (this.renderer) {
+            this.renderer.render(this.scene, this.camera);
+        }
+    }
+
+    resize() {
+        const w = this.canvas.clientWidth;
+        const h = this.canvas.clientHeight;
+        const aspectRatio = w / h;
+
+        if (this.camera) {
+            this.camera.camera.aspect = aspectRatio;
+            this.camera.camera.updateMatrix();
+        }
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.querySelector('canvas');
+    const app = new App(canvas);
+    const gui = new dat.GUI();
+    gui.add(app, 'enableCamera');
+});
